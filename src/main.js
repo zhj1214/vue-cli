a3f6f1a98f1825d0b64cc7802e2c1bacfff49970
zhj1214 <zhj1214@hotmail.com>
2021-03-27 10:11:24 +0800

    解决环境报错

--- src/main.js
+++ src/main.js
@@ -4,55 +4,12 @@
  * @Autor: zhj1214
  * @Date: 2021-03-01 11:44:49
  * @LastEditors: zhj1214
- * @LastEditTime: 2021-03-25 15:27:32
+ * @LastEditTime: 2021-03-26 18:32:17
  */
 import Vue from "vue";
 import App from "./App";
 import store from "./store";
 import uView from "uview-ui";
-
-
-
-
-
-
-// let DIR_NAME = 'test'
-// let fileName = 'test.json'
-// let filePath = './test/test.json'
-// let dirEntry =  file.getDirEntry(DIR_NAME); //创建、获取文件夹
-// let fileEntry =  file.getFileEntry(fileName, dirEntry); // 创建、获取文件
-// let {result:content} =  file.getFileContext(filePath); // 获取文件的内容
-// // let trajectory = JSON.parse(content||"[]");
-// // trajectory.push({lat, lng});
-// fileEntry.createWriter(function(writer){
-// 	writer.seek(0);
-// 	writer.write(JSON.stringify({"a":'112132145434'}));
-// })
-
-
-// production: "https://crm.cntpy.com", // 测试环境
-//     development: "https://htmostest.data4truth.com", // 正式环境
-//     pord: "https://crmuat.cntpy.com", // 预发布
-
-// {
-//   "extEnable": true,
-//   "extAppid": "wxc91325214d05e6e1",
-//   "ext": {
-//     "version": "2.6.0",
-//     "orgId": "12226364",
-//     "appId": "wxc91325214d05e6e1"
-//   }
-// }
-
-
- 
-// fs.mkdir('/home', function() {
-//     fs.writeFile('/home/hello-world.txt', 'Hello world!\n', function() {
-//         fs.readFile('/home/hello-world.txt', 'utf-8', function(err, data) {
-//             console.log(data);
-//         });
-//     });
-// });
 
 
 Vue.use(uView);
