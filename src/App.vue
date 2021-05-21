77e867a8135bd4464771b2ffd2142f899f89b323
zhj1214 <zhj1214@hotmail.com>
2021-05-21 15:21:15 +0800

    feat: app.vue数理

--- src/App.vue
+++ src/App.vue
@@ -1,228 +1,230 @@
 <script>
-import Vue from "vue";
-import api from "./utils/api";
-import local from "./utils/localStorage";
-import toast from "./utils/toast";
-import statistics from "./utils/sampling/statistics";
-import sampling from "./utils/sampling/sampling";
-import util from "./utils/util";
-import date from "./utils/h5utils/date";
-import theme from "./utils/theme";
-import eventEmitter from "./utils/base/EventEmitter.js";
-import canvas from "./utils/canvas";
-import scene from "./utils/sceneManage";
-// import pageR from './utils/base/routePage';
-import navigation from "./utils/base/navrefactor";
+  import Vue from 'vue'
+  import api from './utils/api'
+  import local from './utils/localStorage'
+  import toast from './utils/toast'
+  import statistics from './utils/sampling/statistics'
+  import sampling from './utils/sampling/sampling'
+  import util from './utils/util'
+  import date from './utils/base/date'
+  import theme from './utils/theme'
+  import eventEmitter from './utils/base/EventEmitter.js'
+  import canvas from './utils/canvas'
+  import scene from './utils/sceneManage'
+  // import pageR from './utils/base/routePage';
+  import navigation from './utils/base/navrefactor'
 
-var fundebug = require("fundebug-wxjs");
+  var fundebug = require('fundebug-wxjs')
 
-export default {
-  globalData: {
-    fundebug: fundebug,
-  },
-  onShow: function(options) {
-    console.log(options, "____onShow");
-    this.globalData.onShowOptions = options; // 存储入口参
-  },
-  /**
-   * 运行介绍
-   * 1.加载使用的工具类的模块
-   * 2.初始化 所需要使用的模块
-   * 3.获取小程序模板配置、并配置rootOrgId
-   * 4.调用小程序login、获取openID；检查小程序是否需要更新；保存手机信息
-   * 5.小程序逻辑处理
-   * 6.是否需要 上报错误日志（默认不需要）
-   * */
-  onLaunch: function(options) {
-    console.log(options, "____onLaunch"); // 获取本地当前环境变量value
-    if (process.env.NODE_ENV != "production") {
-      fundebug.init({
-        apikey:
-          "bc54da06260628ce66655e75364fbc0085580b61ef0fc0d0a764555126d06bb2",
-        httpTimeout: 6000,
-      });
-    }
-
-    // setTimeout(() => {
-    //   fundebug.test("Hello", "This is a Test1111111111111122111111133333");
-
-    //   fundebug.notify("Test", "Hello, Fundebug!22222222223");
-    // }, 2000);
-
-    this.globalData.onLaunchOptions = options;
-    // 手动初始化 uniCloud
-    // const myCloud = uniCloud.init({
-    // 	provider: 'tencent',
-    // 	spaceId: 'zhj1214-525887',
-    // 	clientSecret: ''
-    // });
-    this.globalData.isEnableCloud = false;
-    //  绑定全局属性
-    // uni.$uniCloud = myCloud;
-    uni.$api = api;
-    uni.$alert = toast;
-    uni.$sampling = sampling;
-    uni.$statistics = statistics;
-    uni.$util = util;
-    uni.$theme = theme;
-    uni.$localStorage = local;
-    uni.$eventEmitter = eventEmitter;
-    uni.$scene = scene;
-
-    // 初始化 观察者
-    eventEmitter.constructor();
-    uni.$canvas = canvas;
-
-    // 重写
-    // uni.navigateTo = (param,block)=>{
-    //     if(block){
-    //     console.log('进来了')
-    //     return
-    //   }
-    //   wx.navigateTo(param)
-    // }
-
-    // 第三方平台，自定义配置
-    if (uni.getExtConfig) {
-      var self = this;
-      uni.getExtConfig({
-        success(res) {
-          // console.log(res.extConfig, 'getExtConfig')
-          if (res.extConfig) {
-            self.globalData.configExt = res.extConfig;
-            let rootOrgId = "";
-            if (res.extConfig.appId) {
-              self.globalData.appletId = res.extConfig.appId;
-              local.setItem("appletId", res.extConfig.appId);
-              // self.initCloud()
-              // 配置rootOrgId
-              if (res.extConfig.orgId) {
-                rootOrgId = res.extConfig.orgId;
-              }
-            }
-            self.globalData.rootOrgId = rootOrgId;
-            local.setItem("rootOrgId", rootOrgId);
-            // 获取设备信息
-            self.getCurrentIphoneInfo();
-          }
-        },
-      });
-    }
-  },
-  /**
-   * @description: 小程序进入后台
-   * @author: zhj1214
-   */
-  onHide: function() {
-    console.log("App Hide");
-  },
-  methods: {
+  export default {
+    globalData: {
+      fundebug: fundebug,
+    },
+    onShow: function (options) {
+      console.log(options, '____onShow')
+      this.globalData.onShowOptions = options // 存储入口参
+    },
     /**
-     * 获取手机型号、版本信息
-     */
-    getCurrentIphoneInfo() {
-      let iphoneInfo = uni.getSystemInfoSync();
-      let accountInfo = uni.getAccountInfoSync();
-      iphoneInfo = { ...iphoneInfo, ...accountInfo };
-      this.globalData.iphoneInfo = iphoneInfo;
+     * 运行介绍
+     * 1.加载使用的工具类的模块
+     * 2.初始化 所需要使用的模块
+     * 3.获取小程序模板配置、并配置rootOrgId
+     * 4.调用小程序login、获取openID；检查小程序是否需要更新；保存手机信息
+     * 5.小程序逻辑处理
+     * 6.是否需要 上报错误日志（默认不需要）
+     * */
+    onLaunch: function (options) {
+      console.log(options, '____onLaunch') // 获取本地当前环境变量value
+      this.initFundebug() // 初始化 fundebug
+
+      // this.initCloud(); // 手动初始化 uniCloud
+
+      //  绑定全局属性
+      uni.$api = api
+      uni.$alert = toast
+      uni.$sampling = sampling
+      uni.$statistics = statistics
+      uni.$util = util
+      uni.$theme = theme
+      uni.$localStorage = local
+      uni.$eventEmitter = eventEmitter
+      uni.$scene = scene
+      uni.$canvas = canvas
+      // 初始化 观察者
+      eventEmitter.constructor()
+
       // #ifdef MP-WEIXIN
-      //导航高度
-      this.globalData.navHeight = iphoneInfo.statusBarHeight + 46;
-      const version = iphoneInfo.SDKVersion;
-      if (uni.$util.compareVersion(version, "2.5.2") >= 0) {
-        this.iphoneInfo = iphoneInfo; // 根据 model 进行判断 手机型号
-        if (iphoneInfo.model.search("iPhone X") != -1) {
-          this.isIPX = true;
-        }
-        this.dynamicNetWorking().then(() => {
-          uni.$alert.showModal("网络异常", "请检查当前网络是否可用");
-        });
-        this.userLogin();
-        this.appIsUptate();
-      } else {
-        // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
-        uni.showModal({
-          title: "提示",
-          content:
-            "当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。",
-        });
-      }
+      this.getThirdpartyConfig() // 小程序 第三方平台，自定义配置
       // #endif
     },
-    // #ifdef MP-WEIXIN
     /**
-     * 获取 code、openid
+     * @description: 小程序进入后台
+     * @author: zhj1214
      */
-    userLogin() {
-      uni.login({
-        success: (res) => {
-          if (!uni.$localStorage.getItem("userOpenId"))
-            uni.$api
-              .apiRequest("getOpenId", {
-                code: res.code,
-              })
-              .then((res) => {
-                if (res.code == 10000 && res.data.openid) {
-                  this.globalData.userOpenId = res.data;
-                  uni.$localStorage.setItem("userOpenId", res.data.openid);
-                } else {
-                  res.data && uni.$alert.showToast(res.data.message);
+    onHide: function () {
+      console.log('App Hide')
+    },
+    methods: {
+      /**
+       * @description: 初始化 fundebug
+       * @author: zhj1214
+       */
+      initFundebug() {
+        if (process.env.NODE_ENV != 'production') {
+          fundebug.init({
+            apikey: 'bc54da06260628ce66655e75364fbc0085580b61ef0fc0d0a764555126d06bb2',
+            httpTimeout: 6000,
+          })
+        }
+      },
+      /**
+       * @description: 初始化 云环境
+       * @author: zhj1214
+       */
+      initCloud() {
+        const myCloud = uniCloud.init({
+          provider: 'tencent',
+          spaceId: 'zhj1214-525887',
+          clientSecret: '',
+        })
+        uni.$uniCloud = myCloud
+        this.globalData.isEnableCloud = false
+      },
+      /**
+       * @description: 获取 小程序 第三方授权配置
+       * @author: zhj1214
+       */
+      getThirdpartyConfig() {
+        if (uni.getExtConfig) {
+          var self = this
+          uni.getExtConfig({
+            success(res) {
+              // console.log(res.extConfig, 'getExtConfig')
+              if (res.extConfig) {
+                self.globalData.configExt = res.extConfig
+                let rootOrgId = ''
+                if (res.extConfig.appId) {
+                  self.globalData.appletId = res.extConfig.appId
+                  local.setItem('appletId', res.extConfig.appId)
+                  // self.initCloud()
+                  // 配置rootOrgId
+                  if (res.extConfig.orgId) {
+                    rootOrgId = res.extConfig.orgId
+                  }
                 }
-              });
-        },
-      });
-    },
-    /**
-     * 动态监听 网络
-     * */
-    dynamicNetWorking: function() {
-      return new Promise((resolve, reject) => {
-        uni.onNetworkStatusChange(function(res) {
-          if (res.networkType == "none" || res.networkType == "unknown") {
-            reject(false);
-          } else {
-            resolve(true);
+                self.globalData.rootOrgId = rootOrgId
+                local.setItem('rootOrgId', rootOrgId)
+                // 获取设备信息
+                self.getCurrentIphoneInfo()
+              }
+            },
+          })
+        }
+      },
+      /**
+       * 获取手机型号、版本信息
+       */
+      getCurrentIphoneInfo() {
+        let iphoneInfo = uni.getSystemInfoSync()
+        const accountInfo = uni.getAccountInfoSync()
+        iphoneInfo = { ...iphoneInfo, ...accountInfo }
+        this.globalData.iphoneInfo = iphoneInfo
+        // #ifdef MP-WEIXIN
+        //导航高度
+        this.globalData.navHeight = iphoneInfo.statusBarHeight + 46
+        const version = iphoneInfo.SDKVersion
+        if (uni.$util.compareVersion(version, '2.5.2') >= 0) {
+          this.iphoneInfo = iphoneInfo // 根据 model 进行判断 手机型号
+          if (iphoneInfo.model.search('iPhone X') != -1) {
+            this.isIPX = true
           }
-        });
-      });
-    },
-    /**
-     * 小程序更新操作
-     */
-    appIsUptate: function() {
-      const updateManager = uni.getUpdateManager();
-      updateManager.onCheckForUpdate(function(res) {
-        console.log("版本信息 是否需要更新 ：" + res.hasUpdate); // 请求完新版本信息的回调
-      });
-      updateManager.onUpdateReady(function() {
-        uni.showModal({
-          title: "更新提示",
-          content: "新版本已经准备好，需要重启应用？",
-          showCancel: false,
-          success: function(res) {
-            if (res.confirm) {
-              updateManager.applyUpdate(); // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+          this.dynamicNetWorking().then(() => {
+            uni.$alert.showModal('网络异常', '请检查当前网络是否可用')
+          })
+          this.userLogin()
+          this.appIsUptate()
+        } else {
+          // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
+          uni.showModal({
+            title: '提示',
+            content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。',
+          })
+        }
+        // #endif
+      },
+      /**
+       * 获取 code、openid
+       */
+      userLogin() {
+        uni.login({
+          success: (res) => {
+            if (!uni.$localStorage.getItem('userOpenId')) {
+              uni.$api
+                .apiRequest('getOpenId', {
+                  code: res.code,
+                })
+                .then((res) => {
+                  if (res.code == 10000 && res.data.openid) {
+                    this.globalData.userOpenId = res.data
+                    uni.$localStorage.setItem('userOpenId', res.data.openid)
+                  } else {
+                    res.data && uni.$alert.showToast(res.data.message)
+                  }
+                })
             }
           },
-        });
-      });
-      updateManager.onUpdateFailed(function() {
-        uni.$alert.showModal("更新提示", "新版本下载失败"); // 新的版本下载失败
-      });
+        })
+      },
+      /**
+       * 动态监听 网络
+       * */
+      dynamicNetWorking: function () {
+        return new Promise((resolve, reject) => {
+          uni.onNetworkStatusChange(function (res) {
+            if (res.networkType == 'none' || res.networkType == 'unknown') {
+              reject(false)
+            } else {
+              resolve(true)
+            }
+          })
+        })
+      },
+      /**
+       * 小程序更新操作
+       */
+      appIsUptate: function () {
+        const updateManager = uni.getUpdateManager()
+        updateManager.onCheckForUpdate(function (res) {
+          console.log('版本信息 是否需要更新 ：' + res.hasUpdate) // 请求完新版本信息的回调
+        })
+        updateManager.onUpdateReady(function () {
+          uni.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，需要重启应用？',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                updateManager.applyUpdate() // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              }
+            },
+          })
+        })
+        updateManager.onUpdateFailed(function () {
+          uni.$alert.showModal('更新提示', '新版本下载失败') // 新的版本下载失败
+        })
+      },
     },
-    // #endif
-  },
-};
+  }
 </script>
 
 <style>
-/*每个页面公共css */
-@import "./style/style.css";
+  /*每个页面公共css */
+  @import './style/style.css';
 </style>
 <style lang="scss">
-/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
-@import "uview-ui/index.scss";
+  /* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+  @import 'uview-ui/index.scss';
 </style>
 <style lang="less">
-@import "./style/custom.less";
+  @import './style/custom.less';
 </style>
--- src/utils/h5utils/util.js
+++ src/utils/h5utils/util.js
@@ -5,8 +5,8 @@
 
 /** URL 解析 **/
 
-let URL = require("url");
-let urlFormat = require("querystring");
+const URL = require('url')
+const urlFormat = require('querystring')
 
 const Utils = {
   /*************************************************工具类 属性******************************************************************/
@@ -15,64 +15,34 @@ const Utils = {
   /**
    * @description 判断一个value 是否为空
    */
-  isEmpty: function(val) {
+  isEmpty: function (val) {
     if (val) {
       if (val !== null) {
-        if (typeof val === "string") {
+        if (typeof val === 'string') {
           if (val.length == 0) {
-            return true;
+            return true
           }
         }
       } else {
-        return true;
+        return true
       }
     } else {
-      return true;
+      return true
     }
-    return false;
+    return false
   },
-  /**
-   * @description 千分位
-   */
-  numberFormat(num) {
-    var value = num + "";
-    var reg = /\d{1,3}(?=(\d{3})+$)/g;
-    let prefix = "";
-    let suffix = "";
-    // 前后缀
-    if (value.indexOf("+") != -1 || value.indexOf("-") != -1) {
-      prefix = value.substring(0, 1);
-    }
-    if (value.indexOf(".") != -1) {
-      suffix = value.substring(value.indexOf("."), value.length);
-    }
-    // 中间内容
-    value = value.substring(
-      prefix.length > 0 ? 1 : 0,
-      suffix.length > 0 ? value.length - suffix.length : value.length
-    );
-    return prefix + value.replace(reg, "$&,") + suffix;
-  },
+
   /**
    * @description 判断当前系统
    */
   h5GetCurrentSystemiOS() {
-    var u = navigator.userAgent;
+    var u = navigator.userAgent
     // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios终端
-    return isiOS;
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/) // ios终端
+    return isiOS
   },
   /** ***********************************************时间******************************************************************/
-  /**
-   * @description 时间戳转时间
-   * @param format 时间格式
-   * @param date   时间戳 默认当前时间
-   * @example (new Date()).Format("YYYY-MM-DD HH:mm:ss.S")
-   * */
-  getTimeFormat(format, date) {
-    let time = date || new Date();
-    return time.Format(format);
-  },
+
   /**
    * @description 计算两个时间  时间间隔
    * @param time1  开始时间 YYYY-MM-DD HH:mm:ss 时间格式  必填
@@ -81,31 +51,27 @@ const Utils = {
   /*eslint prefer-promise-reject-errors: "error"*/
   calculateAtoBtimeDifference(time1, time2 = new Date()) {
     if (this.isEmpty(time1)) {
-      return 0;
+      return 0
     }
-    var self = this;
-    return new Promise(function(resolve, reject) {
+    var self = this
+    return new Promise(function (resolve, reject) {
       if (window.currentiOSSystem) {
-        time2 = time2.Format("YYYY-MM-DD HH:mm:ss");
-        let end = new Date(time2.replace(/-/g, "/")).getTime();
-        let star = new Date(time1.replace(/-/g, "/")).getTime();
+        time2 = time2.Format('YYYY-MM-DD HH:mm:ss')
+        const end = new Date(time2.replace(/-/g, '/')).getTime()
+        const star = new Date(time1.replace(/-/g, '/')).getTime()
         if (!isNaN(end) && !isNaN(star)) {
-          resolve((end - star) / 1000);
-        } 
+          resolve((end - star) / 1000)
+        }
       } else {
-        let d1 = new Date(time1);
-        time2 = time2 ? time2 : new Date();
-        let d2 = new Date(time2);
-        let difference = parseInt(d2 - d1);
-        if (
-          difference != undefined &&
-          difference != null &&
-          !isNaN(difference)
-        ) {
-          resolve(difference / 1000);
+        const d1 = new Date(time1)
+        time2 = time2 || new Date()
+        const d2 = new Date(time2)
+        const difference = parseInt(d2 - d1)
+        if (difference != undefined && difference != null && !isNaN(difference)) {
+          resolve(difference / 1000)
         }
       }
-    });
+    })
   },
 
   /** 参数说明：
@@ -117,55 +83,45 @@ const Utils = {
   cutString(str, len) {
     // length属性读出来的汉字长度为1
     if (str.length * 2 <= len) {
-      return str;
+      return str
     }
-    var strlen = 0;
-    var s = "";
+    var strlen = 0
+    var s = ''
     for (var i = 0; i < str.length; i++) {
-      s = s + str.charAt(i);
+      s = s + str.charAt(i)
       if (str.charCodeAt(i) > 128) {
-        strlen = strlen + 2;
+        strlen = strlen + 2
         if (strlen >= len) {
-          return s.substring(0, s.length - 1) + "...";
+          return s.substring(0, s.length - 1) + '...'
         }
       } else {
-        strlen = strlen + 1;
+        strlen = strlen + 1
         if (strlen >= len) {
-          return s.substring(0, s.length - 2) + "...";
+          return s.substring(0, s.length - 2) + '...'
         }
       }
     }
-    return s;
-  },
-
-  /**
-   * @description 获取设备尺寸
-   */
-  getScreenSize() {
-    return {
-      width: document.body.scrollWidth,
-      height: document.body.scrollHeight
-    };
+    return s
   },
   /**
    *  @description url 参数解析
    */
-  getUrlParams: function(names, urlSearch = location.href) {
-    let urlParam = URL.parse(urlSearch);
-    let urlObj = urlFormat.parse(urlParam.query);
+  getUrlParams: function (names, urlSearch = location.href) {
+    const urlParam = URL.parse(urlSearch)
+    const urlObj = urlFormat.parse(urlParam.query)
     if (!urlObj[names]) {
-      let hashObj = urlFormat.parse(urlParam.hash);
-      for (let idx in Object.keys(hashObj)) {
-        let key = Object.keys(hashObj)[idx];
-        if (key.indexOf("?") !== -1) {
-          if (key.split("?")[1] == names) {
-            return hashObj[key];
+      const hashObj = urlFormat.parse(urlParam.hash)
+      for (const idx in Object.keys(hashObj)) {
+        const key = Object.keys(hashObj)[idx]
+        if (key.indexOf('?') !== -1) {
+          if (key.split('?')[1] == names) {
+            return hashObj[key]
           }
         }
       }
-      return hashObj[names];
+      return hashObj[names]
     }
-    return urlObj[names];
+    return urlObj[names]
   },
   /**
    * @description 对象去重
@@ -174,29 +130,29 @@ const Utils = {
    * @example this.$Util.objectEquals(x, y) // false
    */
   objectEquals(x, y) {
-    var f1 = x instanceof Object;
-    var f2 = y instanceof Object;
+    var f1 = x instanceof Object
+    var f2 = y instanceof Object
     if (!f1 || !f2) {
-      return x === y;
+      return x === y
     }
     if (Object.keys(x).length !== Object.keys(y).length) {
-      return false;
+      return false
     }
-    var newX = Object.keys(x);
+    var newX = Object.keys(x)
     for (var p in newX) {
-      p = newX[p];
-      var a = x[p] instanceof Object;
-      var b = y[p] instanceof Object;
+      p = newX[p]
+      var a = x[p] instanceof Object
+      var b = y[p] instanceof Object
       if (a && b) {
-        let equal = objectEquals(x[p], y[p]);
+        const equal = objectEquals(x[p], y[p])
         if (!equal) {
-          return equal;
+          return equal
         }
       } else if (x[p] != y[p]) {
-        return false;
+        return false
       }
     }
-    return true;
+    return true
   },
 
   /**
@@ -208,44 +164,44 @@ const Utils = {
   arrayUnique(arr) {
     function unique(x, y) {
       if (isObjArr(x) == -1) {
-        let f = false;
-        x.forEach(item => {
-          objectEquals(item, y) ? (f = true) : false;
-        });
+        let f = false
+        x.forEach((item) => {
+          objectEquals(item, y) ? (f = true) : false
+        })
         if (f) {
-          return x;
+          return x
         } else {
-          let xx = x;
-          let yy = isObjArr(y) == -1 ? y : [y];
-          return [...xx, ...yy];
+          const xx = x
+          const yy = isObjArr(y) == -1 ? y : [y]
+          return [...xx, ...yy]
         }
       } else {
         if (objectEquals(x, y)) {
-          return isObjArr(x) == -1 ? x : [x];
+          return isObjArr(x) == -1 ? x : [x]
         } else {
-          let xx = isObjArr(x) == -1 ? x : [x];
-          let yy = isObjArr(y) == -1 ? y : [y];
-          return [...xx, ...yy];
+          const xx = isObjArr(x) == -1 ? x : [x]
+          const yy = isObjArr(y) == -1 ? y : [y]
+          return [...xx, ...yy]
         }
       }
     }
 
-    var a = [...new Set(arr)];
-    var c = [];
-    var b = a.filter(item => {
-      if (typeof item != "object") {
-        c.push(item);
+    var a = [...new Set(arr)]
+    var c = []
+    var b = a.filter((item) => {
+      if (typeof item !== 'object') {
+        c.push(item)
       }
-      return item ? typeof item == "object" : false;
-    });
-    var d = b.reduce(unique);
-    return !c ? d : !d ? c : [...c, ...d];
+      return item ? typeof item === 'object' : false
+    })
+    var d = b.reduce(unique)
+    return !c ? d : !d ? c : [...c, ...d]
   },
   /**
    * @description 深拷贝 templateData 是要复制的数组或对象，这样的数组或者对象就是指向新的地址的
    */
   copyDeep(templateData) {
-    return JSON.parse(JSON.stringify(templateData));
+    return JSON.parse(JSON.stringify(templateData))
   },
   /**
    * @description 判断一个变量是否是数组;
@@ -254,13 +210,13 @@ const Utils = {
    *  0 value不是数组也不是对象
    * */
   isObjArr(value) {
-    if (Object.prototype.toString.call(value) === "[object Array]") {
-      return -1;
-    } else if (Object.prototype.toString.call(value) === "[object Object]") {
-      return 1;
+    if (Object.prototype.toString.call(value) === '[object Array]') {
+      return -1
+    } else if (Object.prototype.toString.call(value) === '[object Object]') {
+      return 1
     } else {
-      return 0;
+      return 0
     }
   },
-};
-export default Utils;
+}
+export default Utils
