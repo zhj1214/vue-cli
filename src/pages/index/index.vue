9d84a2cd5ca00db8c68a1bcf51ec22846964d87e
sp0366(雍正) <zhj1214@hotmail.com>
2022-10-29 14:33:50 +0800

    init

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Description: 
+ * @Version: 0.0.1
+ * @Autor: zhj1214
+ * @Date: 2021-11-09 14:51:58
+ * @LastEditors: zhj1214
+ * @LastEditTime: 2022-01-19 09:39:10
+-->
 <template>
   <view class="pageHome">
     <navBar title="首页" background="#fff"></navBar>
@@ -12,7 +20,7 @@
       @cancel="okAlert(0)"
     >
     </alertView>
-    
+
     <alertView
       v-if="mallAlertShow"
       :showHeader="false"
@@ -24,82 +32,72 @@
 </template>
 
 <script>
+  import alertView from '../../pageComponents/alertView/alertView'
 
-import alertView from "../../pageComponents/alertView/alertView";
-
-const SCENE_PARSE_KEY = "$_scene_parsed"; // 场景析构
-export default {
-  components: {
-
-    alertView,
-  },
-  /**
-   * 首页逻辑处理
-   * 1.是否需要选择商场
-   * 2.不同商场的场景模式，弹框切换商场
-   * 2.场景调用下一步
-   * 3.进入首页就获取用户信息，更新积分值
-   * */
-
-  data() {
-    return{
-      alertShow: false,
-      mallAlertShow: false
-    }
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   * 没有options可以拿到
-   */
-  onShow: function() {
-   
-  },
-  onLoad(options) {
-    if (options) this.options = options;
-
-  },
-
-  async onReachBottom() {
-    // 如果还有数据
-    if (this.total > this.list.length) {
-      this.params.page = this.params.page + 1;
-      this.fetchList();
-      return;
-    }
-    // 已经是最后一页
-    uni.showToast({
-      title: "没有更多了",
-      icon: "none",
-    });
-  },
-  async onPullDownRefresh() {
-    this.params.page = 1; // current page = 1
-    try {
-      await this.fetchList();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      uni.stopPullDownRefresh();
-    }
-  },
-
-  onPageScroll(obj) {
-    this.opacity = obj.scrollTop / 100;
-  },
-
- methods:{
-
-
- },
-  filters: {
-    priceFormat(val) {
-      if (!val) return "--";
-      return `￥${uni.$util.decimalTwo(val, true, 2)}`;
+  const SCENE_PARSE_KEY = '$_scene_parsed' // 场景析构
+  export default {
+    components: {
+      alertView,
     },
-  },
-};
+    /**
+     * 首页逻辑处理
+     * 1.是否需要选择商场
+     * 2.不同商场的场景模式，弹框切换商场
+     * 2.场景调用下一步
+     * 3.进入首页就获取用户信息，更新积分值
+     * */
+
+    data() {
+      return {
+        alertShow: false,
+        mallAlertShow: false,
+      }
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     * 没有options可以拿到
+     */
+    onShow: function () {},
+    onLoad(options) {
+      if (options) this.options = options
+    },
+
+    async onReachBottom() {
+      // 如果还有数据
+      if (this.total > this.list.length) {
+        this.params.page = this.params.page + 1
+        this.fetchList()
+        return
+      }
+      // 已经是最后一页
+      uni.showToast({
+        title: '没有更多了',
+        icon: 'none',
+      })
+    },
+    async onPullDownRefresh() {
+      this.params.page = 1 // current page = 1
+      try {
+        await this.fetchList()
+      } catch (error) {
+        console.error(error)
+      } finally {
+        uni.stopPullDownRefresh()
+      }
+    },
+
+    onPageScroll(obj) {
+      this.opacity = obj.scrollTop / 100
+    },
+
+    methods: {},
+    filters: {
+      priceFormat(val) {
+        if (!val) return '--'
+        return `￥${uni.$util.decimalTwo(val, true, 2)}`
+      },
+    },
+  }
 </script>
 
-<style lang="less">
-
-</style>
+<style lang="less"></style>
